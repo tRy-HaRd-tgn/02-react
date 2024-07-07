82cf09acb2eeb0da3cfe514c6a81bc8c530ea666
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-07-08 01:49:18 +0300

    приступаем к поиску

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
-import MySelect from "./UI/select";
+import MySelect from "./UI/select/MySelect";
 import PostForm from "./components/PostForm";
 function App() {
+  const [selectedSort, setSelectedSort] = useState("");
   const [posts, setPosts] = useState([
     { id: 1, title: "Javascript", body: "Description" },
     { id: 2, title: "Javascript 2", body: "Description" },
@@ -15,10 +16,28 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+  const sortPost = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])))
+  };
   return (
     <div className="App">
       <PostForm create={createNewPost}></PostForm>
-      <MySelect></MySelect>
+      <MySelect
+        value={selectedSort}
+        onChange={sortPost}
+        defaultValue="сортировка по"
+        options={[
+          {
+            value: "title",
+            name: "по названию",
+          },
+          {
+            value: "body",
+            name: "по описанию",
+          },
+        ]}
+      ></MySelect>
       <PostList
         remove={removePost}
         posts={posts}
--- src/UI/select/MySelect.jsx
+++ src/UI/select/MySelect.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const MySelect = ({ options, defaultValue }) => {
+const MySelect = ({ options, defaultValue, value, onChange }) => {
   return (
-    <select>
-      <option value={""}>{defaultValue}</option>
+    <select value={value} onChange={(event) => onChange(event.target.value)}>
+      <option disabled value={""}>
+        {defaultValue}
+      </option>
       {options.map((option) => (
-        <option value={option.value}>{option.name}</option>
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
       ))}
     </select>
   );
