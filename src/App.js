a9c32c2a1392ccaa66f8e2fb5af7b9370154e9cb
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-07-12 15:27:52 +0300

    написал кастомный хук

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React, { useRef, useMemo, useState } from "react";
+import React, { useState } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import Modal from "./UI/MyModal.jsx/Modal";
 import MyButton from "./UI/button/Mybutton";
+import { usePosts } from "./hooks/usePosts";
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript 2", body: "Description" },
-    { id: 3, title: "Javascript 3", body: "Description" },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
+
   const createNewPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(true)
+    setModal(true);
   };
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLocaleLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const [modal, setModal] = useState("false");
   return (
     <div className="App">
