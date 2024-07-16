dd1a1b93b889a65603a7ab4022992306b0a2721b
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-07-16 17:34:20 +0300

    приступаю к постраничному выводу

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import PostList from "./components/PostList";
 import "./styles/App.css";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
-import Modal from "./UI/MyModal.jsx/Modal";
+import Modal from "./UI/Modal/Modal";
 import MyButton from "./UI/button/Mybutton";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
+import Loader from "./UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [modal, setModal] = useState("false");
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   const createNewPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,14 +26,7 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [modal, setModal] = useState("false");
 
-  async function fetchPosts() {
-    const posts = await PostService.getAll();
-
-    setPosts(posts);
-  }
   useEffect(() => {
     fetchPosts();
   }, []);
@@ -44,11 +45,25 @@ function App() {
       >
         Добавить пост
       </MyButton>
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={"Посты про JS"}
-      ></PostList>
+      {postError && <h1>произошла ошибка ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Loader></Loader>
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title={"Посты про JS"}
+        ></PostList>
+      )}
     </div>
   );
 }
---
+++ src/UI/Loader/Loader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import classes from "./Loader.module.css";
+export default function Loader(props) {
+  return <div className={classes.loader}></div>;
+}
